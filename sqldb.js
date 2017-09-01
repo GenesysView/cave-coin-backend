75a4acb6c5601511b542e44716bc0ad7eef31212
Miguel Angel Asprilla <maasprilla35@misena.edu.co>
2017-08-31 23:28:21 -0500

    db

--- sqldb.js
+++ sqldb.js
@@ -4,17 +4,8 @@ const mongoose = require('mongoose');
 
 var db = {
     Mongoose:mongoose,
-    mongoose: mongoose.connect('mongodb://localhost/facemappdb')
+    mongoose: mongoose.connect('mongodb://facemapp07:facemapp07@ds119044.mlab.com:19044/heroku_ksns66ss')
 };
 
-var promise = mongoose.createConnection('mongodb://localhost/facemappdb', {
-    useMongoClient: true,
-    /* other options */
-  });
-  promise.then(function(db) {
-      console.log('yes');
-    /* Use `db`, for instance `db.model()`
-  });
-  // Or, if you already have a connection
-  connection.openUri('mongodb://localhost/myapp', { /* options */ });
+
 module.exports = db;
