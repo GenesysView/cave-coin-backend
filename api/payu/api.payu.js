22b8fff295457b623feed7b08757592c05fae717
Miguel Angel Asprilla <maasprilla35@misena.edu.co>
2017-09-23 11:34:48 -0500

    commit

--- api/payu/api.payu.js
+++ api/payu/api.payu.js
@@ -177,7 +177,7 @@ router.get('/getpaymentmethods', function (req, res) {
                         "transaction": {
                            "order": {
                               "accountId": "",
-                              "referenceCode": "TestPayU",
+                              "referenceCode": "0001",
                               "description": "payment test",
                               "language": "es",
                               "signature": "7ee7cf808ce6a39b17481c54f2c57acc",
