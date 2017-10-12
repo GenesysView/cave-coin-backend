245ddff3cfda79dc595c8c76227b16bdb944fb62
Miguel Angel Asprilla <maasprilla35@misena.edu.co>
2017-10-11 21:18:42 -0500

    commit

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ db.mongoose
     console.log('Connection has been established successfully.');
   })
   .catch(function (err) {
-    console.log('Unable to connect to the database:', err);
+    console.log('Unable to connect to the daaatabase:', err);
   });
 
 module.exports = app;
